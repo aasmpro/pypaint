ecc40830ea75597814e5b9b63148ce2a5bf1a558
aasmpro <aa.smpro@gmail.com>
2019-01-23 14:15:57 +0330

    adding design buttons

--- paint.py
+++ paint.py
@@ -16,14 +16,11 @@ class Paint(Frame):
         self.line_width = 1
         self.color1 = 'black'
         self.color2 = 'white'
-        self.eraser_on = False
 
         tools_frame = Frame(self)
-        tools_frame.pack(fill='x')
+        tools_frame.pack(fill='both')
 
-        self.brush_button = Button(tools_frame, text='brush', command=self.use_brush)
-        self.brush_button.pack(side="left", padx=5, pady=5)
-
+        # color buttons
         self.color1_button = Button(tools_frame, command=self.choose_color1, highlightbackground=self.color1,
                                     background=self.color1, activebackground=self.color1)
         self.color1_button.pack(side="left", padx=0, pady=5)
@@ -35,29 +32,45 @@ class Paint(Frame):
         self.color1_button.bind("<Enter>", self.set_color1_button)
         self.color2_button.bind("<Enter>", self.set_color2_button)
 
-        self.eraser_button = Button(tools_frame, text='eraser', command=self.use_eraser)
-        self.eraser_button.pack(side="left", padx=5, pady=5)
+        # tool buttons
+        self.brush_button = Button(tools_frame, text='\u23FA', command=self.use_brush, height=1, width=1)
+        self.brush_button.pack(side="left", padx=(100, 0), pady=5)
+
+        self.line_button = Button(tools_frame, text='\\', command=self.use_line, height=1, width=1)
+        self.line_button.pack(side="left", padx=0, pady=5)
+
+        self.circle_button = Button(tools_frame, text='\u20DD', command=self.use_circle, height=1, width=1)
+        self.circle_button.pack(side="left", padx=0, pady=5)
+
+        self.rectangle_button = Button(tools_frame, text='\u20DE', command=self.use_rectangle, height=1, width=1)
+        self.rectangle_button.pack(side="left", padx=0, pady=5)
+
+        self.eraser_button = Button(tools_frame, text='\u232B', command=self.use_eraser, height=1, width=1)
+        self.eraser_button.pack(side="left", padx=0, pady=5)
 
         self.choose_size_button = Scale(tools_frame, from_=1, to=20, orient=HORIZONTAL, command=self.size)
         self.choose_size_button.pack(side="left", padx=5, pady=5)
 
+        # save button
         self.save_button = Button(tools_frame, text='save', command=self.save)
-        self.save_button.pack(side="left", padx=5, pady=5)
+        self.save_button.pack(side="right", padx=0, pady=5)
 
+        # canvas
         canvas_frame = Frame(self)
-        canvas_frame.pack(fill='x')
+        canvas_frame.pack(fill='both')
 
         self.canvas = Canvas(canvas_frame, bg='white', width=600, height=600)
-        self.canvas.pack(side="left", padx=5, pady=5)
+        self.canvas.pack(side="left", padx=0, pady=0)
 
+        self.canvas.bind('<B1-Motion>', self.paint)
+        self.canvas.bind('<ButtonRelease-1>', self.reset)
+
+        # PIL image variable
         self.image = Image.new('RGB', (600, 600), 'white')
         self.draw = ImageDraw.Draw(self.image)
 
         self.active_button = self.brush_button
         self.brush_button.config(relief=SUNKEN)
-
-        self.canvas.bind('<B1-Motion>', self.paint)
-        self.canvas.bind('<ButtonRelease-1>', self.reset)
 
         self.pack()
         self.root.mainloop()
@@ -72,9 +85,6 @@ class Paint(Frame):
         self.color2_button['highlightbackground'] = self.color2
         self.color2_button['activebackground'] = self.color2
 
-    def use_brush(self):
-        self.activate_button(self.brush_button)
-
     def choose_color1(self):
         self.eraser_on = False
         self.color1 = askcolor(color=self.color1)[1]
@@ -85,17 +95,28 @@ class Paint(Frame):
         self.color2 = askcolor(color=self.color1)[1]
         self.set_color2_button(None)
 
-    def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
+    def use_brush(self):
+        self.activate_button(self.brush_button)
 
-    def activate_button(self, some_button, eraser_mode=False):
+    def use_line(self):
+        self.activate_button(self.line_button)
+
+    def use_circle(self):
+        self.activate_button(self.circle_button)
+
+    def use_rectangle(self):
+        self.activate_button(self.rectangle_button)
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button)
+
+    def activate_button(self, some_button):
         self.active_button.config(relief=RAISED)
         some_button.config(relief=SUNKEN)
         self.active_button = some_button
-        self.eraser_on = eraser_mode
 
     def paint(self, event):
-        paint_color = 'white' if self.eraser_on else self.color1
+        paint_color = self.color2 if self.active_button == self.eraser_button else self.color1
         if self.old_x and self.old_y:
             self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
                                     width=self.line_width, fill=paint_color,
